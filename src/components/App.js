d48ebb128fb0b68240dd8c3b06b13c865917ab32
yassin mohamed <yassin_mohamed310@outlook.com>
2022-05-15 19:56:34 +0200

    Craete Search Component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+import '../App.css';
+import SearchBar from './SearchBar'
+function App() {
+  return (
+    <div className="App ui container">
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class SearchBar extends Component {
+    state = { term: '' }
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value })
+    };
+    onFormSubmit=(event)=>{
+        event.preventDefault()
+        console.log(event.target.value)
+    }
+    render() {
+        return (
+            <div className="ui segment search-bar">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <label>Search Video</label>
+                    <input
+                        className="field"
+                        type="text"
+                        value={this.state.term}
+                        onChange={this.onInputChange} />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
